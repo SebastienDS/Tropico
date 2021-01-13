73a320b9c91ec4fc6132e8ca7a10510f01648837
SebastienDS <sebastien.dossantos.77@gmail.com>
2021-01-13 14:13:09 +0100

    removed javafx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,6 @@
 module tropico {
-    requires javafx.controls;
-    requires javafx.fxml;
     requires com.google.gson;
 
     opens tropico to com.google.gson;
-    opens tropico.controllers to javafx.fxml;
-    exports tropico.controllers;
+    exports tropico;
 }
